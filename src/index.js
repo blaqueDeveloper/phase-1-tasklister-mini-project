da91e889efef9cea2122f1c7a0ab5982e75c99d3
Blaque <blaque.develop@gmail.com>
2023-06-21 02:07:13 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        let task = document.getElementById('new-task-description').value
+        
+        // function enterTask(task){
+            // let div = document.createElement('div')
+            // div.setAttribute('class', 'todo')
+
+
+            let li = document.createElement('li')
+            li.textContent = `${task} `
+            li.setAttribute('class', 'taskList')
+        
+            let btn = document.createElement('button')
+            btn.textContent = `Delete`
+            btn.setAttribute('class', 'delete')
+
+            btn.addEventListener('click', deleteTask)
+        
+            li.appendChild(btn)
+            document.querySelector('#tasks').appendChild(li)
+            
+        // }
+
+        form.reset
+    })
 });
+
+function deleteTask(event){
+  event.target.parentNode.remove()
+}
